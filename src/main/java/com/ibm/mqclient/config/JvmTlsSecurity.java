ca27e2036601fa462a9e7a4b0a1f6b2094a69780
gkovan <gkovan@hotmail.com>
2021-04-29 08:06:56 -0400

    code cleanup

--- src/main/java/com/ibm/mqclient/config/JvmTlsSecurity.java
+++ src/main/java/com/ibm/mqclient/config/JvmTlsSecurity.java
@@ -1,7 +1,5 @@
 package com.ibm.mqclient.config;
 
-import java.nio.file.Paths;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -42,21 +40,23 @@ public class JvmTlsSecurity {
 	
     @PostConstruct
     public void init() {
-        // do something
-    	LOG.debug("GK Initializing the application");
-
-        String filename="result.csv";
-        java.nio.file.Path pathToFile = Paths.get(filename);
-        System.out.println(pathToFile.toAbsolutePath());
+    	
+    	// Note: not using profiles yet.  But this code can tell you what profile is active.
+        String activeProfile = ProfileManager.getActiveProfile();
+        LOG.debug("Active profile is: {}", activeProfile );
         
+    	LOG.debug("Initializing the application");
+        
+        LOG.debug("Setting the javax.net.ssl.keyStore from keyStore file: {}", keyStore );
+
         // For mTLS, this MQ client app needs to send a certificate to the MQM server
 		System.setProperty("javax.net.ssl.keyStore", keyStore);
 		System.setProperty("javax.net.ssl.keyStorePassword", keyStorePassword);
 		
         
-        String activeProfile = ProfileManager.getActiveProfile();
-        LOG.debug("Active profile is: {}", activeProfile );
+
         
+        LOG.debug("Setting the javax.net.ssl.trustStore from keyStore file: {}", trustStore );
         
         // The trust store has the CA certificate that signed the MQM server certificate.
         // This is required to setup a TLS connection with the MQM server
