9363aad52d155a899a08dca067c6f3d74ba85e9f
gkovan <gkovan@hotmail.com>
2021-04-30 17:05:30 -0400

    remove usused code

--- src/main/java/com/ibm/mqclient/MQClientResource.java
+++ src/main/java/com/ibm/mqclient/MQClientResource.java
@@ -64,47 +64,4 @@ public class MQClientResource {
         return new Status();
     }
 	
-//	@Inject
-//	@ConfigProperty(name = "app.keystore", defaultValue = "passw0rd")
-//    private String keyStore;
-//	
-//	@Inject
-//	@ConfigProperty(name = "app.keystore.pw", defaultValue = "passw0rd")
-//    private String keyStorePassword;
-//	
-//	@Inject
-//	@ConfigProperty(name = "app.truststore", defaultValue = "passw0rd")
-//    private String trustStore;
-//	
-//	@Inject
-//	@ConfigProperty(name = "app.truststore.pw", defaultValue = "passw0rd")
-//    private String trustStorePassword;
-    
-    @PostConstruct
-    void init() {
-        // do something
-    	LOG.debug("GK Initializing the application");
-
-        String filename="result.csv";
-        java.nio.file.Path pathToFile = Paths.get(filename);
-        System.out.println(pathToFile.toAbsolutePath());
-        
-        // For mTLS, this MQ client app needs to send a certificate to the MQM server
-//		System.setProperty("javax.net.ssl.keyStore", keyStore);
-//		System.setProperty("javax.net.ssl.keyStorePassword", keyStorePassword);
-		
-        
-        String activeProfile = ProfileManager.getActiveProfile();
-        LOG.debug("Active profile is: {}", activeProfile );
-        
-        
-        // The trust store has the CA certificate that signed the MQM server certificate.
-        // This is required to setup a TLS connection with the MQM server
-//		System.setProperty("javax.net.ssl.trustStore", trustStore);
-//		System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
-
-
-		System.setProperty("com.ibm.mq.cfg.useIBMCipherMappings", "false");
-    	
-    }
 }
